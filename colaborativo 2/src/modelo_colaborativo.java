16a600ad93c1771d0e0acb3dad0f5cd7c4e2128b
usuario <usuario@usuario-HP-Compaq-6000-Pro-MT-PC>
2016-11-28 20:56:01 -0500

    Este es mi sexto importe a git hub

--- colaborativo 2/src/modelo_colaborativo.java
+++ colaborativo 2/src/modelo_colaborativo.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,32 @@
  * @author usuario
  */
 public class modelo_colaborativo {
+    public int n1,n2;
+
+    public modelo_colaborativo(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public int getN1() {
+        return n1;
+    }
+
+    public void setN1(int n1) {
+        this.n1 = n1;
+    }
+
+    public int getN2() {
+        return n2;
+    }
+
+    public void setN2(int n2) {
+        this.n2 = n2;
+    }
     
+    public void suma(){
+        int r=0;
+        r=this.n1 + this.n2;
+        JOptionPane.showMessageDialog(null, "La suma es: "+r);
+    }
 }
